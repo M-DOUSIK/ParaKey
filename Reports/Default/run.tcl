3753cca42beca936bb3aa0d9323433c4cf1dd121
M-DOUSIK <m.dousik@gmail.com>
2025-11-05 16:26:08 +0530

    Updated Default Keypad

--- Reports/Default/run.tcl
+++ Reports/Default/run.tcl
@@ -2,11 +2,11 @@ set_db init_lib_search_path /home/install/FOUNDRY/digital/45nm/LIBS/lib/max
 set_db lef_library /home/install/FOUNDRY/digital/45nm/LIBS/lef/gsclib045.fixed.lef
 set_db library slow.lib
 
-read_hdl {./../../Default/Source/KeyPadInterpreter.v}
+read_hdl {./top.v}
 
 elaborate
 
-read_sdc ./constraints_KeyPadInterpreter.sdc
+read_sdc ./constraints_top.sdc
 
 
 
@@ -14,12 +14,12 @@ syn_generic
 syn_map
 syn_opt
 
-write_hdl > KeyPadInterpreter_netlist.v
-write_sdc > KeyPadInterpreter_sdc.sdc
+write_hdl > top_netlist.v
+write_sdc > top_sdc.sdc
 
-report power > KeyPadInterpreter_power.rpt
-report area > KeyPadInterpreter_area.rpt
-report gates > KeyPadInterpreter_gates.rpt
-report timing -unconstrained > KeyPadInterpreter_timing.rpt
+report power > top_power.rpt
+report area > top_area.rpt
+report gates > top_gates.rpt
+report timing > top_timing.rpt
 
 gui_show
